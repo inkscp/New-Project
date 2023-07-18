bd816cbc3885e24f24a139d742795657e1dc84e3
Inna <inna.pshenichnikova@gmail.com>
2023-07-18 15:43:13 +0300

    asyncio

--- main.py
+++ main.py
@@ -11,21 +11,62 @@ from flask import Flask
 from multiprocessing import Process  # симулировали каждый вызов ф-ции отдельным процессом, параллельным.так можно ф-ции(приложения) запускать параллельно.минус - нагрузка на операционку
 import threading
 
-def print_name(prefix):
-    print(f'Ищем префикс {prefix}')
-    try:
-        while True:
-            name = (yield)
-            if prefix in name:
-                print(name)
-    except GeneratorExit:
-        print('Корутина (coroutine) завершена')
+"""
+Asyncio - модуль асинхронного программирования, который был представлен в Питон. Все карутины содержат точки и ты переключаемся между ними
+"""
+import signal, sys, json, asyncio, aiohttp
 
-corou = print_name('Уважаемый')
-corou.__next__()
-corou.send('товарищ')
-corou.send('Уважаемый товарищ')
-corou.close()
+loop = asyncio.get_event_loop()  # все будет выполняться независимо и асинхронно
+client = aiohttp.ClientSession(loop=loop)
+
+async def get_json(client, url):
+    async with client.get(url) as response:
+        assert response.status == 200  # утверждение. если не так - выбросится искл-е
+        return await response.read()
+
+
+async def get_reddit_top(subreddit, client):
+    url = 'https://www.reddit.com/r/' + subreddit + '/top.json?sort=top&t=day&limit=5'
+    print(url)
+    data1 = await get_json(client, url)
+    j = json.loads(data1.decode('utf-8'))
+    for i in j['data']['children']:
+        score = i['data']['score']
+        title = i['data']['title']
+        link = i['data']['url']
+        print(str(score) + ': ' + title + ' (' + link + ')')
+    print('Готово:', subreddit + '\n')
+
+    def signal_handler(signal, frame):
+        loop.stop()
+        client.close()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # asyncio.ensure_future(get_reddit_top('python', 'client'))
+    asyncio.ensure_future(get_reddit_top('programming', 'client'))
+    asyncio.ensure_future(get_reddit_top('compsci', 'client'))
+    loop.run_forever()
+
+
+
+
+# def print_name(prefix):
+#     print(f'Ищем префикс {prefix}')
+#     try:
+#         while True:
+#             name = (yield)
+#             if prefix in name:
+#                 print(name)
+#     except GeneratorExit:
+#         print('Корутина (coroutine) завершена')
+#
+# corou = print_name('Уважаемый')
+# corou.__next__()
+# corou.send('товарищ')
+# corou.send('Уважаемый товарищ')
+# corou.close()
 
 
 
