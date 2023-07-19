991f8777a3a44a23fa08d17f6bb22c9aa44ac5c0
Inna <inna.pshenichnikova@gmail.com>
2023-07-19 15:15:27 +0300

    telegram - bot3 ECHO(async)

--- main.py
+++ main.py
@@ -14,50 +14,145 @@ import threading
 import telebot
 from telebot import types  # для указания типов
 import config
+import logging
+from telegram.ext import Application, MessageHandler, filters
 
+
+# бот №3 (асинхронный)
+# запустим логгирование
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+"""
+Функция обработки сообщений 
+updater - принимает
+context - доп инф-я о сообщении
+что мы ему напишем, то он нам в ответ и отправляет
+"""
+async def echo(update, context):  #любое текстовое сообщение будет вызывать функцию
+    await update.message.reply_text(update.message.text)
+
+def main():
+    application = Application.builder().token(config.bot_token).build()
+    text_handler = MessageHandler(filters.TEXT, echo)  # бот может получать другие обработчики - команды, inline запросы
+    application.add_handler(text_handler)
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
+
+# бот №1
 # /start
 
 # инициировали бота
-bot = telebot.TeleBot(config.bot_token)
+# bot = telebot.TeleBot(config.bot_token)
+#
+#
+# @bot.message_handler(commands=['start'])  # реагируем на '/start'
+# def start(message):
+#     # создаем кнопку-ссылку. 0.first_name - то, как ты зарегистрированы в телеге
+#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+#     btn1 = types.KeyboardButton('Поздороваться')
+#     btn2 = types.KeyboardButton('Задать вопрос')
+#     markup.add(btn1, btn2)  # добавляем в контейнер кнопки, так мы их регистрируем
+#     bot.send_message(message.chat.id, text='Привет, {0.first_name}! '
+#                                            'Я бот, приятно познакомиться)'.format(message.from_user),
+#                      reply_markup=markup)  # это я
+#
+# @bot.message_handler(content_types=['text'])
+# def func(message):
+#     if message.text == "Поздороваться":
+#         bot.send_message(message.chat.id, text='Привет. Спасибо. что зашел!')
+#     elif message.text == "Задать вопрос":
+#         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+#         btn1 = types.KeyboardButton('Как меня зовут?')
+#         btn2 = types.KeyboardButton('Что я умею?')
+#         back = types.KeyboardButton('Назад в главное меню')
+#         markup.add(btn1, btn2, back)
+#         bot.send_message(message.chat.id, text='Задайте мне вопрос', reply_markup=markup)
+#     elif message.text == 'Как меня зовут?':
+#         bot.send_message(message.chat.id, text='Я всего лишь бот')
+#     elif message.text == 'Что я умею?':
+#         bot.send_message(message.chat.id, text='Здороваться')
+#     elif message.text == 'Назад в главное меню':
+#         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+#         button1 = types.KeyboardButton('Поздороваться')
+#         button2 = types.KeyboardButton('Задать вопрос')
+#         markup.add(button1, button2)
+#         bot.send_message(message.chat.id, text='Вы вернулись в главное меню', reply_markup=markup)
+#     else:
+#         bot.send_message(message.chat.id, text='Я не знаю такой команды')
+#
+# # запустили бота
+# bot.polling(non_stop=True)  # будет работать пока мы его не остановим
 
 
-@bot.message_handler(commands=['start'])  # реагируем на '/start'
-def start(message):
-    # создаем кнопку-ссылку. 0.first_name - то, как ты зарегистрированы в телеге
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton('Поздороваться')
-    btn2 = types.KeyboardButton('Задать вопрос')
-    markup.add(btn1, btn2)  # добавляем в контейнер кнопки, так мы их регистрируем
-    bot.send_message(message.chat.id, text='Привет, {0.first_name}! '
-                                           'Я бот, приятно познакомиться)'.format(message.from_user),
-                     reply_markup=markup)  # это я
 
-@bot.message_handler(content_types=['text'])
-def func(message):
-    if message.text == "Поздороваться":
-        bot.send_message(message.chat.id, text='Привет. Спасибо. что зашел!')
-    elif message.text == "Задать вопрос":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton('Как меня зовут?')
-        btn2 = types.KeyboardButton('Что я умею?')
-        back = types.KeyboardButton('Назад в главное меню')
-        markup.add(btn1, btn2, back)
-        bot.send_message(message.chat.id, text='Задайте мне вопрос', reply_markup=markup)
-    elif message.text == 'Как меня зовут?':
-        bot.send_message(message.chat.id, text='Я всего лишь бот')
-    elif message.text == 'Что я умею?':
-        bot.send_message(message.chat.id, text='Здороваться')
-    elif message.text == 'Назад в главное меню':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton('Поздороваться')
-        button2 = types.KeyboardButton('Задать вопрос')
-        markup.add(button1, button2)
-        bot.send_message(message.chat.id, text='Вы вернулись в главное меню', reply_markup=markup)
-    else:
-        bot.send_message(message.chat.id, text='Я не знаю такой команды')
-
-# запустили бота
-bot.polling(non_stop=True)  # будет работать пока мы его не остановим
+# # бот №2
+# bot = telebot.TeleBot(config.bot_token)
+# name = ''
+# surname = ''
+# age = 0
+#
+# @bot.message_handler(content_types=['text'])
+# def start(message):
+#     if message.text == '/start':
+#         bot.send_message(message.from_user.id, "Как Вас зовут?")
+#         # мы регистрируем следующий шаг юзера
+#         bot.register_next_step_handler(message, get_name)
+#     else:
+#         bot.send_message(message.from_user.id,
+#                          'Я Вас понял, напишите /start')
+# def get_name(message):
+#     # даем разрешение ф-ции
+#     global name
+#     name = message.text
+#     bot.send_message(message.from_user.id, "Как Ваша фамилия?")
+#     bot.register_next_step_handler(message, get_surname)
+# def get_surname(message):
+# #даем разрешение ф-ции
+#     global surname
+#     surname = message.text
+#     bot.send_message(message.from_user.id, "Сколько Вам лет?")
+#     bot.register_next_step_handler(message, get_age)
+#
+# def get_age(message):
+# #даем разрешение ф-ции
+#     global age
+#     while age == 0:  #удостоверимся. что поменялся возраст
+#         try:
+#             age = int(message.text)
+#         except ValueError:
+#             bot.send_message(message.from_user.id, "Введите цифры")
+#             age = 1
+#             break
+#     question = f'Тебе {age} лет, тебя зовут {name} {surname}?'
+#     keyboard = types.InlineKeyboardMarkup()  # клавиатуру добавили
+#     yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
+#     keyboard.add(yes)  # зарегистрировали
+#     no = types.InlineKeyboardButton(text='Нет', callback_data='no')
+#     keyboard.add(no)
+#     bot.send_message(message.from_user.id, text = question, reply_markup=keyboard)
+#
+# @bot.callback_query_handler(func=lambda call: True)  # прописываем реакцию на то. что мы ответили
+# def callback_worker(call):
+#     """
+#     call.data - это callback_data, которую мы указали при объявлении кнопки
+#     """
+#     if call.data == "yes":
+#         # тут либо сохраняем, либо обрабатываем данные
+#         bot.send_message(call.message.chat.id,
+#                          'Приятно познакомиться')
+#     elif call.data == "no":
+#         # инициируем диалог повторно
+#         bot.send_message(call.message.chat.id,
+#                          'Тогда начнём сначала... Как тебя зовут?')
+#         bot.register_next_step_handler(call.message, get_name)
+#
+# bot.polling(non_stop=True)
 
 # """
 # Asyncio - модуль асинхронного программирования, который был представлен в Питон. Все карутины содержат точки и ты переключаемся между ними
